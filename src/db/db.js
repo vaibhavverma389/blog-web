e0433ae52489978cea513b2421243d30e67f412d
vaibhav verma <vaibhavverma6393@gmail.com>
2025-07-31 21:20:43 +0530

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,6 @@ require("dotenv").config(); // Load .env file
 const connect = () => {
   mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log("✅ Connected to MongoDB Atlas"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
